f8312bb4ba24175798b6cbc28768ed0a979f9eb5
PavanKumarReddyDandu <pavankumardandu.tr@gmail.com>
2020-02-03 21:40:01 -0600

    updated

--- Source/Sourcemain.java
+++ Source/Sourcemain.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Sourcemain {
     public static void main(String[] args) throws IOException {
         BattleshipSearch battleshipSearch=new BattleshipSearch(); //object for Battleship Search class
-        battleshipSearch.readFile("C:\\Users\\pavan\\Desktop\\coord.txt"); //calling readFile method
+        battleshipSearch.readFile("Assignment1/input.txt"); //calling readFile method
     }
 
 }
