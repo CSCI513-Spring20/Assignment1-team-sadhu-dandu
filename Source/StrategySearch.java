fd3bc175e80b290267f58707e8247f9f723f78db
PavanKumarReddyDandu <pavankumardandu.tr@gmail.com>
2020-02-05 18:33:59 -0600

    Updated

--- Source/StrategySearch.java
+++ Source/StrategySearch.java
@@ -1,4 +1,8 @@
 package Source;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class StrategySearch implements SearchMethods {
     @Override
@@ -6,7 +10,16 @@ public class StrategySearch implements SearchMethods {
         int cnt=0;
         int cells=0;
         String coordinates="";
-        for(int p=0;p<name.length;p++) {
+        /*ArrayList<String> list = new ArrayList<String>();
+        for(int i=0;i<25;i++) {
+            for (int j = 0; j < 25; j++) {
+
+                list.add(name[i][j]);
+                */
+
+
+
+         for(int p=0;p<name.length;p++) {
             for(int j=0;j<name[0].length;j++) {
                 if (name[j][p]=="C" || name[j][p]=="B"){
                     cnt++;
